1d76d04a212d090406ecfbe7e6a24d51cb529989
og-python-scripts <47901278+og-python-scripts@users.noreply.github.com>
2019-02-22 10:20:04 -1000

    Create pg.90.end=.sep=.py

---
+++ pg.90.end=.sep=.py
@@ -0,0 +1,30 @@
+print('Hello')
+print('World')
+
+'''
+the output would look like this:
+Hello
+World
+'''
+
+print('Hello', end='')
+print('World')
+
+'''
+the output would look like this:
+HelloWorld
+'''
+
+print('cats', 'dogs', 'mice')
+
+
+'''
+cats dogs mice
+'''
+
+print('cats', 'dogs', 'mice', sep=',')
+
+'''
+cats,dogs,mice
+'''
+
