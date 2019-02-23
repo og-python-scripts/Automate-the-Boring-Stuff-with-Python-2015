5013efb12deb6abea3ab679a08f7f8a563af4e96
og-python-scripts <47901278+og-python-scripts@users.noreply.github.com>
2019-02-22 15:02:41 -1000

    Create pg.105.individualvalues.lists.py

---
+++ pg.105.individualvalues.lists.py
@@ -0,0 +1,16 @@
+spam = ['cat', 'bat', 'rat', 'elephant']
+spam[0]
+'cat'
+spam[1]
+'bat'
+spam[2]
+'rat'
+spam[3]
+'elephant'
+
+['cat', 'bat', 'rat', 'elephant'][3]
+'elephant'
+'Hello ' + spam[0]
+'Hello cat'
+'The ' + spam[1] + ' ate the ' + spam[0] + '.'
+'The bat ate the cat.'
